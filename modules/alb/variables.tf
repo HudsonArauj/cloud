8dae5010c53020be3443c6dc4d6695b5679405db
HudsonArauj <hudsonma@al.insper.edu>
2023-11-30 23:20:26 -0300

    Stop tracking modules/ec2/ and modules/alb/

--- modules/alb/variables.tf
+++
@@ -1,3 +0,0 @@
-variable application_name {}
-variable vpc_id {}
-variable public_subnet_ids {}
--- modules/ec2/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "application_name" {}
-variable "public_subnet_a" {}
-variable "public_subnet_b" {}
-variable "vpc_id" {}
-variable "ami_id" {
-    default = "ami-0fc5d935ebf8bc3bc"
-}
